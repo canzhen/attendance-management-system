e4dc21cb96547bf71055cda9ee1a709a9386ff1f
Kinney <1141201865@qq.com>
2015-12-11 17:45:28 +0800

    no message

--- src/action/teacherAction.java
+++ src/action/teacherAction.java
@@ -5,14 +5,15 @@ import java.util.Map;
 public class teacherAction extends MyActionSupport{
 	private Map request;
 	/**
-	 * 在返回页面之前，需要从数据库中比对，查找当前要上的课，
-	 * 然后把课程信息放到request里传递过去。
-	 * 若当前时间段没有课，那么就返回NOCURRENTCLASS，跳转到无课页面。
+	 * 鍦ㄨ繑鍥為〉闈箣鍓嶏紝闇�瑕佷粠鏁版嵁搴撲腑姣斿锛屾煡鎵惧綋鍓嶈涓婄殑璇撅紝
+	 * 鐒跺悗鎶婅绋嬩俊鎭斁鍒皉equest閲屼紶閫掕繃鍘汇��
+	 * 鑻ュ綋鍓嶆椂闂存娌℃湁璇撅紝閭ｄ箞灏辫繑鍥濶OCURRENTCLASS锛岃烦杞埌鏃犺椤甸潰銆�
 	 */
+
 	@Override
 	public String index(){
 		request = getRequest();
-		request.put("test", new Integer(1));//类似这样传值过去
+		request.put("test", new Integer(1));//绫讳技杩欐牱浼犲�艰繃鍘�
 		return SUCCESS;
 	}
 }
